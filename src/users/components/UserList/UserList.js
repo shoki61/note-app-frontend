3a448e93f9cb6c4854b2ba20b2aec8e69df4f4b8
Sohrat <sohrat6128@gmail.com>
2021-02-19 18:43:51 +0300

    set spinner to user list component and change spinner view

--- src/users/components/UserList/UserList.js
+++ src/users/components/UserList/UserList.js
@@ -1,20 +1,21 @@
 import React from 'react';
 
 
-import UserItem from './UserItem/UserItem'
+import UserItem from './UserItem/UserItem';
+import Spinner from '../../../shared/components/Spinner/Spinner';
 import './UserList.css';
 
 const UserList = props => {
     console.log(props.data)
     return(
-        <div style={{width:'100%'}}>
+        <div style={{width:'100%', display:'flex', flexDirection:'column', alignItems:'center'}}>
             {props.data.length ? props.data.map(user => <UserItem
                 key={user._id}
                 id={user._id}
                 name={user.name}
                 image={user.image}
                 notes={user.notes.length}
-             />):<p>Loading...</p>}
+             />):<Spinner/>}
         </div>
     );
 };
