282b1417b825b867f7ab86b2fb68391013ef60fa
Sohrat Jumadurdyyev <sohrat6128@gmail.com>
2021-03-04 17:55:55 +0300

    fix modal component

--- src/shared/components/Modal/Modal.js
+++ src/shared/components/Modal/Modal.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
+
+import Button from '../Button/Button';
+import Backdrop from '../Backdrop/Backdrop';
 import './Modal.css';
 
-const Modal = props => {
-    <div className='Modal'>
-        {props.children}
+const Modal = props => (
+    <div className='modal'>
+        <div className='modal-content'>
+            <Button onClick={props.closeModal} className='modal-close-button'><i className='fa fa-close'></i></Button>
+            {props.children}
+        </div>
+        <Backdrop onClose={props.closeModal} />
     </div>
-};
+);
 
 export default Modal;
